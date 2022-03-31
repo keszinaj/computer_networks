c80eab822a65c0c09d333848e323d0efd7fecf7f
keszinaj <kubajaniszek123@gmail.com>
2022-03-31 20:09:17 +0200

    first compile without errors

--- traceroute/receive.c
+++ traceroute/receive.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <errno.h>
 
-int check_correctnes(int fd, int id, int seq, struct icmp* icmp_header, u_int8_t *icmp_len)
+int check_correctnes(int id, int seq, struct icmp* icmp_header, u_int8_t *icmp_len)
 {
     if(icmp_header->icmp_type != ICMP_TIME_EXCEEDED && icmp_header->icmp_type != ICMP_ECHOREPLY)
         return 0;  
@@ -65,7 +65,7 @@ int receive(int fd, int id, int seq)
         struct ip *ip_header = (struct ip*)buffer;
         u_int8_t *icmp_len = buffer + 4 * ip_header->ip_hl;
         struct icmp *icmp_header = (struct icmp*) icmp_len;
-        pom= check_correctnes(fd, id, seq, icmp_header, icmp_len);
+        pom= check_correctnes(id, seq, icmp_header, icmp_len);
         if(pom == 1)
         {
            // printf("AAAAA|n");
@@ -109,8 +109,10 @@ int receive(int fd, int id, int seq)
             if(strcmp(sender_ip_str[0], sender_ip_str[2])==0)
                 printf("%s %d\n", sender_ip_str[0], time_to_responde);
             else
+            {
                 printf("%s ?\n", sender_ip_str[0]);
-                printf("%s ?\n", sender_ip_str[2]);
+                printf("%s ?\n", sender_ip_str[2]); ///eeee an pewno?
+            }
         }
         else if(strcmp(sender_ip_str[0], sender_ip_str[2])==1 && strcmp(sender_ip_str[1], sender_ip_str[2])==1)
         {
