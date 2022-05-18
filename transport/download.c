468bcec535ae33ea1f7a2d588dc44d585f61858a
keszinaj <kubajaniszek123@gmail.com>
2022-05-18 20:00:49 +0200

    add firts implementation of window

--- transport/download.c
+++ transport/download.c
@@ -6,8 +6,30 @@
 #include <unistd.h>
 #include <errno.h>
 
-int get()
+// implementacj samego okna
+// TO TRZEBA DEFINE!!!!
+int const window_size = 1000;
+int const packet_length = 1000;
+int free_space = window_size;
+int start;
+int filled[window_size] = {0};
+char window[window_size][packet_length] ={0};
+int next()
 {
+    if(start == window_size-1)
+    {
+        return 0;
+    }
+    else
+    {
+        return start+1;
+    }
+}
+int get(size_t start, size_t length)
+{
+    char request_str[100];
+    sprintf(request_str, "GET %ld %ld\n", start, length);
+    int check = sendto(sockfd, message, message_len, 0, (struct sockaddr*) &server_address, sizeof(server_address)) != message_len)
 
 }
 int send_request()
@@ -18,7 +40,7 @@ receive_data()
 {
     
 }
-int download()
+int download(int size)
 {
     // na podsawiw wykładu 4
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -37,10 +59,25 @@ int download()
         fprintf(stderr, "Error: Wrong Ip address"); 
         return EXIT_FAILURE;
     }
+    int amount_of_packets = 1;
+    int next_in_window = 0;
+    int amount_of_packet_to_send =1;
+    //pre config
+    if(size>1000)
+    {
+        amount_of_packets = ceil(size/1000);//sufit 
+        if(amount_of_packets > 1000)
+        {
+            next_in_window = 1000;
+        }
+    }
     int downloaded = 0;
     while(downloaded < size)
     {
-        send_request();
+        while(amount_of_packet_to_send)
+        {
+            send_request()
+        }
         receive_data();
     }
 }
---
+++ transport/window.c
@@ -0,0 +1,67 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int const window_size = 1000;
+int const packet_length = 1000;
+int free_space = window_size;
+int filled[window_size] = {0};
+char window[window_size][packet_length] ={0};
+int first_to_save = 0;
+int last_to_save = 0;
+int all_memory_square = 1;
+int start_cash= 0;
+int length_of_last_packet = 0;
+int proba(int size)
+{
+    //preprocesing dla pierwszej pętli while
+    if(size<packet_length)
+    {
+        length_of_last_packet = size;
+    }
+    else
+    {
+        int cash_needed = ceil(size/1000);
+        if(cash_needed>window_size)
+        {
+            last_to_save = window_size - 1;
+            all_memory_square = window_size;
+        }
+        else
+        {
+            last_to_save = cash_needed - 1;
+            all_memory_square = cash_needed;
+        }
+        length_of_last_packet = size%packet_length;
+        if(length_of_last_packet == 0)
+        {
+            length_of_last_packet = packet_length;
+        }
+    }
+
+    int j,i;
+    while(size>0)
+    {
+        for(i =0; i<all_memory_square; i++)
+        {
+            j = first_to_save+i;
+            if(j>=window_size)
+            {
+               j = j - window_size;
+            }
+            if(filled[j] == 0)
+            {
+                get(start_cash+i*packet_length, packet_length);
+            }
+            if(filled[j] > 0)
+            {
+                continue;
+            }
+            //ostatni pakiet
+            if(filled[j] == -1)
+            {
+                continue;
+            }
+        }
+
+    }
+}
