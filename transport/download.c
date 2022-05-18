7779fb9aa8f3c334b6f7de5f8587dafeba0c3164
keszinaj <kubajaniszek123@gmail.com>
2022-05-19 01:32:03 +0200

    zarys recv

--- transport/download.c
+++ transport/download.c
@@ -11,7 +11,7 @@
 int const window_size = 1000;
 int const packet_length = 1000;
 int free_space = window_size;
-int start;
+int start_global = 0; //głupia nazwa
 int filled[window_size] = {0};
 char window[window_size][packet_length] ={0};
 int next()
@@ -25,19 +25,50 @@ int next()
         return start+1;
     }
 }
-int get(size_t start, size_t length)
+
+int send_request(size_t start, size_t length)
 {
     char request_str[100];
     sprintf(request_str, "GET %ld %ld\n", start, length);
-    int check = sendto(sockfd, message, message_len, 0, (struct sockaddr*) &server_address, sizeof(server_address)) != message_len)
+    int check = sendto(sockfd, request_str, message_len, 0, (struct sockaddr*) &server_address, sizeof(server_address)) != message_len);
+    if(check< 0)
+    {
+        printf("add error msg");
+    }
 
 }
-int send_request()
+receive_data(int number_of_sent)
 {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(fd, &readfds);
+    struct timeval timeout;
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+     char *buffer = malloc(1000);
+    while(number_of_sent > 0)
+    {
+        int ready = select(fd+1, &readfds, NULL, NULL, &timeout);
+        //chceck if timeout
+        if(ready == 0)
+        {
+            break;
+        }
+        if(ready < 0){
+            fprintf(stderr, "Error: Select error %s\n", strerror(errno));
+            return EXIT_FAILURE;
+         }
+         else{
+            ssize_t packet_len = recvfrom (fd, buffer, IP_MAXPACKET, 0, (struct sockaddr*)&sender, &sender_len);
+		    if (packet_len < 0) {
+			    fprintf(stderr, "recvfrom error: %s\n", strerror(errno)); 
+			    return EXIT_FAILURE;
+            }
+            //check czy to dobre ip
+            
+         }
 
-}
-receive_data()
-{
+    }
     
 }
 int download(int size)
@@ -59,6 +90,9 @@ int download(int size)
         fprintf(stderr, "Error: Wrong Ip address"); 
         return EXIT_FAILURE;
     }
+
+
+
     int amount_of_packets = 1;
     int next_in_window = 0;
     int amount_of_packet_to_send =1;
@@ -66,9 +100,14 @@ int download(int size)
     if(size>1000)
     {
         amount_of_packets = ceil(size/1000);//sufit 
-        if(amount_of_packets > 1000)
+        
+        else if(amount_of_packets > 1000)
         {
             next_in_window = 1000;
+            amount_of_packet_to_send= 1000;
+        }
+        else{
+            amount_of_packet_to_send= amount_of_packets
         }
     }
     int downloaded = 0;
@@ -76,7 +115,7 @@ int download(int size)
     {
         while(amount_of_packet_to_send)
         {
-            send_request()
+            send_request(start_global, next_in_window);
         }
         receive_data();
     }
